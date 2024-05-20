9cfa2cc33f289b7c0d2e940f37de7bdf76290104
DreamingJohnny <johnnydreaming@gmail.com>
2024-05-20 17:11:54 +0200

    CardZones highlight on mouse over

--- Lotka Volterra Card Game/Assets/00_Content/Scripts/ZoneSelection.cs
+++ Lotka Volterra Card Game/Assets/00_Content/Scripts/ZoneSelection.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class ZoneSelection : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler {
 
@@ -10,12 +11,21 @@ public class ZoneSelection : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
 	//Once we've fixed that, the next step should be to have it send an event when the mouse clicks on it,
 	//So that the GM, that subscribes to the event, then knows what it is, and can decide if it should recieve the card.
 
+	[SerializeField][Range(0f,1.0f)] private float neutralAlpha = 0.35f;
+	[SerializeField][Range(0f, 1.0f)] private float highlightedAlpha = 1.0f;
+
 	public event Action<ZoneSelection> OnZoneSelection;
 
+	private Image image;
 
+	private void Awake() {
+		image = GetComponentInChildren<Image>();
+		Debug.Assert(image != null);
+		image.color = new Color(image.color.r,image.color.g,image.color.b,neutralAlpha);
+	}
 
 	public void OnPointerEnter(PointerEventData pointerEventData) {
-		Debug.Log("Pointer enters...");
+		image.color = new Color(image.color.r, image.color.g, image.color.b, highlightedAlpha);
 	}
 
 	public void OnPointerClick(PointerEventData pointerEventData) {
@@ -23,5 +33,6 @@ public class ZoneSelection : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
 		OnZoneSelection?.Invoke(this);
 	}
 	public void OnPointerExit(PointerEventData pointerEventData) {
+		image.color = new Color(image.color.r, image.color.g, image.color.b, neutralAlpha);
 	}
 }
